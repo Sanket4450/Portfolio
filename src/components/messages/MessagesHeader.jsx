e62362637ade4c6e6432ca26f0905d2b062a5cd8
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-03-16 22:21:30 +0530

    fixed some ui bugs in messages component

--- src/components/messages/MessagesHeader.jsx
+++ src/components/messages/MessagesHeader.jsx
@@ -1,14 +1,15 @@
 const SingleHeaderSection = ({ name, width }) => {
-  return <div className={` ${width} text-center`}>{name}</div>
+  return <div className={`${width} text-center`}>{name}</div>
 }
 
 export const MessagesHeader = ({ screenWidth }) => {
   return (
     <div className=" flex items-center h-11 bg-text-theme-primary text-bg-primary text-lg lg:text-xl font-semibold">
-      <SingleHeaderSection name="Sender Name" width={screenWidth >= 768 ? 'w-[20%]' : 'w-[25%]'} />
-      <SingleHeaderSection name="Email" width="w-[30%]" />
-      <SingleHeaderSection name="Date Received" width={screenWidth >= 768 ? 'w-[25%]' : 'w-[30%]'} />
-      <SingleHeaderSection name="Status" width="w-[15%]" />
+      <div className=" w-[3%]" />
+      <SingleHeaderSection name="Sender Name" width={screenWidth >= 768 ? 'w-[17%]' : 'w-[27%]'} />
+      <SingleHeaderSection name="Email" width={screenWidth >= 768 ? 'w-[35%]' : 'w-[40%]'} />
+      <SingleHeaderSection name="Date Received" width={screenWidth >= 768 ? 'w-[20%]' : 'w-[30%]'} />
+      {screenWidth >= 768 && <SingleHeaderSection name="Status" width="w-[15%]" />}
       {screenWidth >= 768 && <SingleHeaderSection name="Actions" width="w-[10%]" />}
     </div>
   )
--- src/components/messages/SingleMessage.jsx
+++ src/components/messages/SingleMessage.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReply, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faReply, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export const SingleMessage = ({ messageId, firstName, lastName, email, isRead, receivedAt, screenWidth }) => {
   if (screenWidth >= 640) {
@@ -8,12 +8,15 @@ export const SingleMessage = ({ messageId, firstName, lastName, email, isRead, r
         id={messageId}
         className=" flex items-center h-10 text-primary border-b-4 border-text-theme-primary hover:cursor-pointer"
       >
-        <p className=" w-[25%] md:w-[20%] text-center">
+        <div className=" w-[3%] text-center">
+          {!isRead && <FontAwesomeIcon icon={faCircle} className=" text-theme-primary text-[12px]" />}
+        </div>
+        <p className=" w-[27%] md:w-[17%] text-center">
           {firstName} {lastName}
         </p>
-        <p className=" w-[30%] text-center">{email}</p>
-        <p className=" w-[30%] md:w-[25%] text-center">{receivedAt.toDateString()}</p>
-        <p className=" w-[15%] text-center">{isRead ? 'Read' : 'Unread'}</p>
+        <p className=" w-[40%] md:w-[35%] text-center">{email}</p>
+        <p className=" w-[30%] md:w-[20%] text-center">{receivedAt.toDateString()}</p>
+        {screenWidth >= 768 && <p className=" w-[15%] text-center">{isRead ? 'Read' : 'Unread'}</p>}
         {screenWidth >= 768 && (
           <div className=" w-[10%] text-center space-x-[15%]">
             <FontAwesomeIcon icon={faReply} className=" text-lg hover:cursor-pointer hover:text-gray-strong" />
@@ -34,11 +37,13 @@ export const SingleMessage = ({ messageId, firstName, lastName, email, isRead, r
           </p>
           <p className=" phone:text-[14px]">{email}</p>
         </div>
-        <div className=" pr-3 phone:h-[85%] max-phone:w-full flex phone:flex-col justify-between max-phone:items-center text-center">
+        <div className=" pr-3 phone:h-[85%] max-phone:w-full py-0.5 flex phone:flex-col justify-between max-phone:items-center text-center">
           <p className=" text-sm max-phone:ml-3">{receivedAt.toDateString()}</p>
-          <p className=" bg-text-theme-hover-primary max-phone:px-4 max-phone:py-1 max-phone:mr-1 text-center font-semibold text-sm rounded-lg">
-            {isRead ? 'Read' : 'Unread'}
-          </p>
+          {!isRead && (
+            <p className=" bg-gray-weak text-primary max-phone:px-4 max-phone:py-1 max-phone:mr-1 text-center font-semibold text-sm rounded-lg">
+              Unread
+            </p>
+          )}
         </div>
       </div>
     )
