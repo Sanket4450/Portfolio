1b09ae4c0f0fed5b7ecf1f63adfeab842740605f
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-03-18 14:07:44 +0530

    added react memo to message field component

--- src/components/messages/MessageField.jsx
+++ src/components/messages/MessageField.jsx
@@ -1,8 +1,13 @@
-export const MessageField = ({ messageKey, messageValue }) => {
+import { useEffect, memo } from 'react'
+
+export const MessageField = memo(({ messageKey, messageValue }) => {
+  useEffect(() => {
+    console.log('MessageField rendered')
+  })
   return (
     <div className=" py-2 px-2 space-y-2.5">
       <p className=" bg-text-theme-primary text-bg-primary text-lg font-semibold rounded-md px-2">{messageKey}</p>
       <p className=" bg-greenGray text-justify rounded-md px-2 overflow-scroll">{messageValue}</p>
     </div>
   )
-}
+})
